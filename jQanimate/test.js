6f755c278cbb3655fe22bac507eb482957196b9c
tejaswigowda <tejaswi@foxyninjastudios.com>
2015-09-14 11:45:57 -0700

    easings added

--- jQanimate/test.js
+++ jQanimate/test.js
@@ -1,5 +1,48 @@
-var buttonClicked = function(n)
+var boxStatus = [
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left",
+  "left"
+];
+
+
+var buttonClicked = function(n, easing)
 {
   var id = "#button" + n.toString();
-  $(id).hide(); 
+  var pageWidth = $("body").width();
+  var boxWidth = 150;
+  if(boxStatus[n] == "left"){
+    $(id).animate({"margin-left" : pageWidth - boxWidth + "px", "color": "red"}, 1000, easing); 
+    boxStatus[n] = "right";
+  }
+  else{
+    $(id).animate({"margin-left" : "0px", "color" : "black"}, 1000, easing); 
+    boxStatus[n] = "left";
+  }
 }
